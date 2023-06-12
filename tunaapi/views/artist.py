2bd3074383b11ef1f23a864714bfa2d38332670c
Wesley Vance <wslyvnce@gmail.com>
2023-06-11 22:52:12 -0500

    finished setting up views for all models and urls

---
+++ tunaapi/views/artist.py
@@ -0,0 +1,81 @@
+from django.http import HttpResponseServerError
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers, status
+from tunaapi.models import Artist
+
+class ArtistView(ViewSet):
+    """Level up artist view"""
+
+    def retrieve(self, request, pk):
+        """Handle GET requests for single artist
+
+        Returns:
+            Response -- JSON serialized artist
+        """
+        try:
+            artist = Artist.objects.get(pk=pk)
+            serializer = ArtistSerializer(artist)
+            return Response(serializer.data)
+        except Artist.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+
+    def list(self, request):
+        """Handle GET requests to get all artists
+
+        Returns:
+            Response -- JSON serialized list of artists
+        """
+        artists = Artist.objects.all()
+
+        serializer = ArtistSerializer(artists, many=True)
+        return Response(serializer.data)
+
+    def create(self, request):
+        """Handle POST operations
+
+        Returns
+            Response -- JSON serialized artist instance
+        """
+
+        artist = Artist.objects.create(
+            name=request.data["name"],
+            age=request.data["age"],
+            bio=request.data["bio"],
+        )
+        serializer = ArtistSerializer(artist)
+        return Response(serializer.data)
+
+    def update(self, request, pk):
+        """Handle PUT requests for a artist
+
+        Returns:
+            Response -- Empty body with 204 status code
+        """
+
+        artist = Artist.objects.get(pk=pk)
+        artist.name = request.data["name"]
+        artist.age = request.data["age"]
+        artist.bio = request.data["bio"]
+        artist.save()
+
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
+
+    def destroy(self, request, pk):
+        """Handle DELETE requests for single artist
+        
+        Returns:
+            Response -- Empty body with 204 status code
+        """
+        artist = Artist.objects.get(pk=pk)
+        artist.delete()
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
+
+
+class ArtistSerializer(serializers.ModelSerializer):
+    """JSON serializer for events
+    """
+    class Meta:
+        model = Artist
+        fields = ('id', 'name', 'age', 'bio')
